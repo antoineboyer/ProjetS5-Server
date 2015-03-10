f687e8e86927340ff7cc67721d8552b40a1831bf
jbferri <jb.ferri@telecom-bretagne.eu>
2015-03-10 14:55:22 +0100

    supression de l'ancien dossier

--- ProjetS51and1/Test/OpenClassRoomJS.php
+++
@@ -1,7 +0,0 @@
-<form method="post" action="traitement.php">
-   <p>
-       <label for="pseudo">Modifier</label>
-       <input type="text" name="pseudo" id="pseudo" />
-       <input type="submit" value="envoyer"/>
-   </p>
-</form>
